47ee857b427fd5883ed8aeb071453697bc0f4333
Suchita Patel <suchipatel5486@gmail.com>
2026-02-19 11:44:42 -0500

    Refactor server configuration and add notes routes
    Updated server to use environment variable for PORT and added notes routes.

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 require("dotenv").config();
 
-
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-console.log("Current directory:", __dirname);
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
-
 app.use(cors());
 app.use(express.json());
 
@@ -19,19 +16,15 @@ const authRoutes = require(path.join(__dirname, "routes", "auth"));
 console.log("Auth routes loaded");
 app.use("/auth", authRoutes);
 
-app.post("/auth/register", (req, res) => {
-    res.send("Route is working!");
-});
-
+const notesRoutes = require("./routes/notes");
+app.use("/notes", notesRoutes);
 
 // Test root
 app.get("/", (req, res) => {
-  res.send("Smart Notes Backend is running!");
+  res.json({ message: "Backend is live!" });
 });
 
+// Start server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
-
-const notesRoutes = require("./routes/notes");
-app.use("/notes", notesRoutes);
